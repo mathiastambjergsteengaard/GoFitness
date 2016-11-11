b4228bd0a43ff707ec51d1f98b2589e4ad6f3001
mathiastambjergsteengaard <mathiastambjergiversen@gmail.com>
2016-11-11 14:09:19 +0100

    rught url string

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,7 +1,9 @@
 var mongoose = require( 'mongoose' );
-autoIncrement = require('mongoose-auto-increment');
+var autoIncrement = require('mongoose-auto-increment');
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
 
-var dbURI = 'mongodb://localhost/gofitness_rest';
+var dbURI = "mongodb://Group24:Group24@ds149577.mlab.com:49577/gofitness;"
 var connection = mongoose.createConnection(dbURI);
 mongoose.connect(dbURI);
 
@@ -25,6 +27,41 @@ var finishedSchema = new mongoose.Schema({
   name: String,
   date: String
 });
+
+var userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  }, name: {
+    type: String,
+    required: true
+  },
+  hash: String,
+  salt: String
+});
+
+userSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
+userSchema.methods.validPassword = function(password) {
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex'); return this.hash === hash;
+};
+
+userSchema.methods.generateJwt = function() {
+  var expiry = new Date();
+  expiry.setDate(expiry.getDate() + 7);
+
+  return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    name: this.name,
+    exp: parseInt(expiry.getTime() / 1000),
+  }, 'thisIsSecret' );
+};
+
 
 programSchema.plugin(autoIncrement.plugin, 'Program');
 var Program = mongoose.model('Program', programSchema);
